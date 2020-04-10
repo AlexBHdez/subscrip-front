5708e690086231d1458d8a2e6b650d19bc7c2a23
alexbhdez <abhbretones@gmail.com>
2020-04-10 11:48:49 +0200

    Update signup form

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import useForm from '../../utils/customHooks'
 import { inject, observer } from 'mobx-react'
+import { Button, Form, FormField } from '../../ui'
 
 const Signup = inject('store')(observer(props => {
   const { signupUser } = props.store
@@ -10,21 +11,12 @@ const Signup = inject('store')(observer(props => {
   const { inputs, handleInputChange, handleSubmit } = useForm(onSubmit)
   
   return (
-    <form onSubmit={ handleSubmit }>
-      <div>
-        <label>Username</label>
-        <input type='text' name='name' required onChange={ handleInputChange } value={ inputs.name || '' } />
-      </div>
-      <div>
-        <label>Email</label>
-        <input type='email' name='email' required onChange={ handleInputChange } value={ inputs.email || '' } />
-      </div>
-      <div>
-        <label>Password</label>
-        <input type='password' name='password' required onChange={ handleInputChange } value={ inputs.password || '' } />
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
+    <Form onSubmit={ handleSubmit }>
+      <FormField label='Username' type='text' name='name' required onChange={ handleInputChange } value={ inputs.name || '' } />
+      <FormField label='Email' type='email' name='email' required onChange={ handleInputChange } value={ inputs.email || '' } />
+      <FormField label='Password' type='password' name='password' required onChange={ handleInputChange } value={ inputs.password || '' } />
+      <Button type='submit'>Sign Up</Button>
+    </Form>
   )
 }))
 
